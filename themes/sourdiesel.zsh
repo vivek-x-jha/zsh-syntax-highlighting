40676420d465213a95176ae7a1c2bb22a74cb687
Vivek Jha <vivek.x.jha@gmail.com>
2024-03-10 23:27:41 -0700

    Added sourdiesel syntax highlighting theme

---
+++ themes/sourdiesel.zsh
@@ -0,0 +1,57 @@
+#!/usr/bin/env zsh
+
+# *******************************************************************************
+# Author: Vivek Jha
+# Last Modified: Mar 10, 2024
+#
+# Custom theme for Zsh-syntax-highlighting plugin
+# TODO visualize hashed command
+# *******************************************************************************
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=13
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=225
+ZSH_HIGHLIGHT_STYLES[alias]=fg=225
+ZSH_HIGHLIGHT_STYLES[suffix-alias]=fg=225
+ZSH_HIGHLIGHT_STYLES[global-alias]=fg=225
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=225
+ZSH_HIGHLIGHT_STYLES[function]=fg=12
+ZSH_HIGHLIGHT_STYLES[command]=fg=225
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=3
+ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=8
+ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=196
+ZSH_HIGHLIGHT_STYLES[autodirectory]=fg=12
+ZSH_HIGHLIGHT_STYLES[path]=fg=12
+ZSH_HIGHLIGHT_STYLES[path_pathseparator]=fg=8
+ZSH_HIGHLIGHT_STYLES[path_prefix]=fg=159
+# ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]=fg=165
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=7
+ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=
+ZSH_HIGHLIGHT_STYLES[command-substitution]=fg=2
+ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]=fg=2
+ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]=fg=2
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]=fg=2
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]=fg=2
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]=fg=2
+ZSH_HIGHLIGHT_STYLES[process-substitution]=fg=2
+ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]=fg=2
+ZSH_HIGHLIGHT_STYLES[arithmetic-expansion]=fg=
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=3
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=3
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=2
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]=fg=13
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=2
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=2
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]=fg=13
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=2
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]=fg=13
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]=fg=2
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]=fg=13
+ZSH_HIGHLIGHT_STYLES[rc-quote]=fg=2
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=7
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=2
+ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]=fg=2
+ZSH_HIGHLIGHT_STYLES[assign]=fg=7
+ZSH_HIGHLIGHT_STYLES[redirection]=fg=7
+ZSH_HIGHLIGHT_STYLES[comment]=fg=8
+ZSH_HIGHLIGHT_STYLES[named-fd]=fg=3
+ZSH_HIGHLIGHT_STYLES[numeric-fd]=fg=3
+ZSH_HIGHLIGHT_STYLES[arg0]=fg=225
