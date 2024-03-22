4cc7c4cebe6b3270604cff93e249d95946d82935
Vivek Jha <vivek.x.jha@gmail.com>
2024-03-22 03:52:06 -0700

    Updated ZSH_HIGHLIGHT_STYLES values

--- themes/sourdiesel.zsh
+++ themes/sourdiesel.zsh
@@ -1,56 +1,58 @@
 #!/usr/bin/env zsh
 
-# ****************************************************************# Author: Vivek Jha
-# Last Modified: Mar 18, 2024
+# ****************************************************************
+# Author: Vivek Jha
+# Last Modified: Mar 21, 2024
 #
 # Custom theme for Zsh-syntax-highlighting plugin
 # TODO visualize hashed command
 # ***************************************************************
-ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=1
-ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=225
-ZSH_HIGHLIGHT_STYLES[alias]=fg=225
-ZSH_HIGHLIGHT_STYLES[suffix-alias]=fg=225
-ZSH_HIGHLIGHT_STYLES[global-alias]=fg=225
-ZSH_HIGHLIGHT_STYLES[builtin]=fg=225
-ZSH_HIGHLIGHT_STYLES[function]=fg=4
-ZSH_HIGHLIGHT_STYLES[command]=fg=225
-ZSH_HIGHLIGHT_STYLES[precommand]=fg=5
-ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=8
-ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=196
-ZSH_HIGHLIGHT_STYLES[autodirectory]=fg=4
-ZSH_HIGHLIGHT_STYLES[path]=fg=4
-ZSH_HIGHLIGHT_STYLES[path_pathseparator]=fg=8
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=red
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=magenta
+ZSH_HIGHLIGHT_STYLES[alias]=fg=green
+ZSH_HIGHLIGHT_STYLES[suffix-alias]=fg=green
+ZSH_HIGHLIGHT_STYLES[global-alias]=fg=green
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=green
+ZSH_HIGHLIGHT_STYLES[function]=fg=green
+ZSH_HIGHLIGHT_STYLES[command]=fg=green
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=magenta
+ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=black
+ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=black
+ZSH_HIGHLIGHT_STYLES[autodirectory]=fg=blue
+ZSH_HIGHLIGHT_STYLES[path]=fg=blue
+ZSH_HIGHLIGHT_STYLES[path_pathseparator]=fg=black
 ZSH_HIGHLIGHT_STYLES[path_prefix]=fg=12
 # ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]=fg=165
-ZSH_HIGHLIGHT_STYLES[globbing]=fg=7
-ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=
-ZSH_HIGHLIGHT_STYLES[command-substitution]=fg=2
-ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]=fg=2
-ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]=fg=2
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]=fg=2
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]=fg=2
-ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]=fg=2
-ZSH_HIGHLIGHT_STYLES[process-substitution]=fg=2
-ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]=fg=2
-ZSH_HIGHLIGHT_STYLES[arithmetic-expansion]=fg=14
-ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=3
-ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=3
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=2
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]=fg=1
-ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=2
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=2
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]=fg=1
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=2
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]=fg=1
-ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]=fg=2
-ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]=fg=1
-ZSH_HIGHLIGHT_STYLES[rc-quote]=fg=2
-ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=7
-ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=2
-ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]=fg=2
-ZSH_HIGHLIGHT_STYLES[assign]=fg=7
-ZSH_HIGHLIGHT_STYLES[redirection]=fg=7
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=white
+ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=magenta
+ZSH_HIGHLIGHT_STYLES[command-substitution]=fg=green
+ZSH_HIGHLIGHT_STYLES[command-substitution-unquoted]=fg=green
+ZSH_HIGHLIGHT_STYLES[command-substitution-quoted]=fg=green
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]=fg=green
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-unquoted]=fg=green
+ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter-quoted]=fg=green
+ZSH_HIGHLIGHT_STYLES[process-substitution]=fg=green
+ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]=fg=green
+ZSH_HIGHLIGHT_STYLES[arithmetic-expansion]=fg=cyan
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-unclosed]=fg=red
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=green
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]=fg=red
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]=fg=red
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]=fg=black
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]=fg=red
+ZSH_HIGHLIGHT_STYLES[rc-quote]=fg=green
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=black
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]=fg=green
+ZSH_HIGHLIGHT_STYLES[assign]=fg=white
+ZSH_HIGHLIGHT_STYLES[redirection]=fg=white
 ZSH_HIGHLIGHT_STYLES[comment]=fg=8
-ZSH_HIGHLIGHT_STYLES[named-fd]=fg=3
-ZSH_HIGHLIGHT_STYLES[numeric-fd]=fg=3
-ZSH_HIGHLIGHT_STYLES[arg0]=fg=225
+ZSH_HIGHLIGHT_STYLES[named-fd]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[numeric-fd]=fg=yellow
+ZSH_HIGHLIGHT_STYLES[arg0]=fg=green
+
